441a8f1fa31057b7bab0cdc68d318fb6dac63c9a
Darren Aquilina <aquid5244@gmail.com>
2016-11-28 17:04:34 +0100

    created new project

---
+++ cmake-build-debug/CMakeFiles/THE_IT_CREW.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/THE_IT_CREW.dir/main.cpp.obj"
+  "THE_IT_CREW.pdb"
+  "THE_IT_CREW.exe"
+  "THE_IT_CREW.exe.manifest"
+  "libTHE_IT_CREW.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/THE_IT_CREW.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
