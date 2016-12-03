c821cb72187150d28807672bb5930be65e3a4b8a
daqu0019 <aquid5244@gmail.com>
2016-12-03 17:36:36 +0100

    Organized files

---
+++ cmake-build-debug/CMakeFiles/THE_IT_CREW.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/THE_IT_CREW.dir/Coding/calculator.c.obj"
+  "THE_IT_CREW.pdb"
+  "THE_IT_CREW.exe"
+  "THE_IT_CREW.exe.manifest"
+  "libTHE_IT_CREW.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/THE_IT_CREW.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/THE_IT_CREW.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for THE_IT_CREW.
+# This may be replaced when dependencies are built.
